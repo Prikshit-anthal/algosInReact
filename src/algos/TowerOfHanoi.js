1605177fa6219dd6af4b955c439cd76b4cd11a5c
Prikshit-anthal <prikshit_12013102@nitkkr.ac.in>
2022-01-06 14:00:25 +0530

    Box and line component created

--- src/algos/TowerOfHanoi.js
+++ src/algos/TowerOfHanoi.js
@@ -1,7 +1,24 @@
 import React from 'react'
-
+import Box from '../components/Box/Box'
+import Line from '../components/Line/Line'
 function TowerOfHanoi() {
-  return <>Tower of Hanoi</>
+  return (
+    <>
+      Tower of Hanoi
+      <Box width='4rem' height='1.5rem' colour="orange"/>
+      <div
+        style={{
+          position: 'relative',
+          width: '200px',
+          height: '500px',
+          backgroundColor: 'pink',
+        }}
+      >
+        
+        <Line  />
+      </div>
+    </>
+  )
 }
 
 export default TowerOfHanoi
